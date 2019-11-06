dfb7a9e4680cc9b5eb94d3f50e5f99ad838dd8cf
Lucas Moura <lsantosdemoura99@gmail.com>
2019-11-06 05:40:12 -0300

    Fixing bugs, adding docstrings(?) and giving better names.

--- clojure/src/classic_algorithms/collatz_conjecture.clj
+++ clojure/src/classic_algorithms/collatz_conjecture.clj
@@ -1,27 +1,33 @@
 (ns classic-algorithms.collatz-conjecture)
 
-(declare calculate-collatz)
+(declare check-number)
 (declare ask-number)
 
 (defn calculate-collatz
+  "Calculates the conjecture and returns the number of iterations were needed to get to 1."
   [number count]
+  (if (not= number 1)
+    (do
+      (if (= (mod number 2) 0)
+        (recur (quot number 2) (+ count 1))
+        (recur (+ (* number 3) 1) (+ count 1))))
+    (println (+ count 1))))
+
+(defn check-number
+  "Checks a number, if it's all right calls a function to calculate the conjecture."
+  [number]
   (if (<= number 0)
     (do
       (println number "is less than or equal to 0, enter another number please:")
       (ask-number))
-    (do
-      (if (not= number 1)
-        (do
-          (if (= (mod number 2) 0)
-            (recur (quot number 2) (+ count 1))
-            (recur (+ (* number 3) 1) (+ count 1))))
-        (println (+ count 1))))))
+    (calculate-collatz number 0)))
 
 (defn ask-number
+  "Asks for the user to enter a number and calls a function to check it."
   []
   (println "Enter a number bigger than 0:")
   (let [entered-number (Long/valueOf (read-line))]
-    (calculate-collatz entered-number 0)))
+    (check-number entered-number)))
 
 (defn -main
   [& args]
--- clojure/src/classic_algorithms/sieve_of_eratosthenes.clj
+++ clojure/src/classic_algorithms/sieve_of_eratosthenes.clj
@@ -1,43 +1,50 @@
 (ns classic-algorithms.sieve-of-eratosthenes)
 
 (declare ask-number)
-(declare calculate-sieve)
+(declare check-number)
 
-(defn not-multiple?
+(defn not-multiple-or-different?
+  "Checks if numbers are different or one is a multiple of the other."
   [multiple number]
   (if (not= multiple number)
     (not= (mod multiple number) 0)
     true))
 
-(defn non-multiples
-  [limit-number start-number integers-list]
-  (println start-number)
-  (if (not= start-number limit-number)
+(defn calculate-sieve
+  "Calculates the sieve through a recursion."
+  [limit-number start-number position integers-list]
+  (if (<= start-number limit-number)
     (do
       (->>
         integers-list
         (filter (fn
                   [multiple]
-                  (not-multiple? multiple start-number)))
-        (recur limit-number (first integers-list))))
-    integers-list))
+                  (not-multiple-or-different? multiple start-number)))
+        (recur limit-number (nth integers-list position) (inc position))))
+    (println integers-list)))
 
-(defn calculate-sieve
+(defn check-number
+  "Checks a number, if it's all right calls a function to calculate the sieve."
   [limit-number]
   (if (<= limit-number 0)
     (do
       (println limit-number "is less than or equal to 0, enter another number please:")
       (ask-number))
     (do
+      ; incrementing 1 to limit-number so the list can include it
       (let [integers-until-limit (range 2 limit-number)
-            biggest-number-to-be-checked (int (Math/floor (Math/sqrt limit-number)))]
-            (non-multiples biggest-number-to-be-checked 2 integers-until-limit)))))
+            biggest-number-to-be-checked (int (Math/floor (Math/sqrt limit-number)))
+            first-number-to-be-searched 2
+            first-numbers-position 1]
+            (calculate-sieve biggest-number-to-be-checked first-number-to-be-searched
+                           first-numbers-position integers-until-limit)))))
 
 (defn ask-number
+  "Asks for the user to enter a number and calls a function to check it."
   []
   (println "Enter a number bigger than 0:")
   (let [entered-number (Long/valueOf (read-line))]
-    (calculate-sieve entered-number)))
+    (check-number entered-number)))
 
 (defn -main
   [& args]
